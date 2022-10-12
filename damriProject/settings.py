09313301476fde5411f183622863861a479f4d58
aries wibowo <riespratama@gmail.com>
2022-10-13 01:50:24 +0700

    whitenoise for heroku deployment

--- damriProject/settings.py
+++ damriProject/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [ BASE_DIR / "static",]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"),]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
