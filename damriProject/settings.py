39483d52ee20c98c0b5ed793227527687c887c04
aries wibowo <riespratama@gmail.com>
2022-10-13 01:56:44 +0700

    whitenoise for heroku deployment

--- damriProject/settings.py
+++ damriProject/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'LayakMengemudi/static'),]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
