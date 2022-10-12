22494430ea191a4d26b423ea932624ef9d598548
aries wibowo <riespratama@gmail.com>
2022-10-13 00:10:12 +0700

    Bootstrap added

--- LayakMengemudi/urls.py
+++ LayakMengemudi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import PengemudiDetail
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pengemudi/<int:pk>/', PengemudiDetail.as_view(), name='pengemudi-detail')
     ]
--- LayakMengemudi/views.py
+++ LayakMengemudi/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, CreateView
+from .models import Pemeriksaan, Pengemudi
 
 def index(request):
     return render(request, 'home.html')
+
+class PengemudiDetail(DetailView):
+    model = Pengemudi
+    template_name = 'pengemudi_detail.html'
+
+class PemeriksaanCreate(CreateView):
+    model = Pemeriksaan
+    template_name = 'pemeriksaan_create.html'
