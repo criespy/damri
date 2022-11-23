e0f9f3c9cadb371cf712c4eef220c3189a103cad
aries wibowo <riespratama@gmail.com>
2022-11-23 22:46:08 +0700

    Delete pattern \d on pemeriksaan form

--- LayakMengemudi/forms.py
+++ LayakMengemudi/forms.py
@@ -59,11 +59,10 @@ class FormPemeriksaanCreate(ModelForm):
         fields = '__all__'
         widgets = {
             'pengemudi': forms.Select({'class': 'form-control form-select'}),
-            'tanggal': forms.TextInput({'class': 'form-control datepicker', 'autocomplete': 'off',
-                                        'value': datetime.now().strftime("%Y-%m-%d %H:%M:%S")}),
-            'tensi': forms.TextInput({'class': 'form-control', 'placeholder': '120/80', 'pattern':'\d*'}),
-            'suhu': forms.TextInput({'class': 'form-control', 'placeholder': '36.1', 'pattern':'\d*'}),
-            'jam_tidur': forms.TextInput({'class': 'form-control', 'placeholder': '8', 'pattern':'\d*'}),
+            'tanggal': forms.TextInput({'class': 'form-control datepicker', 'autocomplete': 'off', 'value': datetime.now().strftime("%Y-%m-%d %H:%M:%S")}),
+            'tensi': forms.TextInput({'class': 'form-control', 'placeholder': '120/80'}),
+            'suhu': forms.TextInput({'class': 'form-control', 'placeholder': '36.1'}),
+            'jam_tidur': forms.TextInput({'class': 'form-control', 'placeholder': '8'}),
             'kondisi': forms.Textarea({'class': 'form-control'}),
             'status': forms.Select({'class': 'form-control form-select'}),
         }
